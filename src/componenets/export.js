9e78ab740ff24220669d6e240c9d1a458c43748d
Meron95 <meron.64@hotmail.com>
2023-07-12 12:01:52 +0200

    first commit

---
+++ src/componenets/export.js
@@ -0,0 +1,6 @@
+export { default as About } from './about/about';
+export { default as Gallery} from './gd/gallery';
+export { default as Models} from './models/models';
+export { default as Modals} from './modals/Modal';
+export { default as Projects} from './projects/projects';
+export { default as Contact} from './contact/contact';
