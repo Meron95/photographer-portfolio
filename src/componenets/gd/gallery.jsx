9bd7d56c9b3a42d507acc69fb8f60c28ee0e4340
Meron95 <36710730+Meron95@users.noreply.github.com>
2023-08-06 18:38:38 +0200

    Update gallery.jsx

--- src/componenets/gd/gallery.jsx
+++ src/componenets/gd/gallery.jsx
@@ -6,31 +6,26 @@ import Modal from "../modals/Modal";
 
 const Gallery = ({ models }) => {
 
+    const { id } = useParams();
+    const selectedModel = models.find(model => model.id === parseInt(id));
+
     // State to store the selected image URL
     const [selectedImage, setSelectedImage] = useState(null);
-
-    // Function to handle image selection
-    const handleImageClick = (image) => {
-        setSelectedImage(image);
-    };
-
-    // Function to clear selected image
-    const clearSelectedImage = () => {
-        setSelectedImage(null);
-    };
+    const [imageIndex, setImageIndex] = useState(0);
+    const [showSlideshow, setShowSlideshow] = useState(false);
 
     // array of images for diffrent models
     const imageArrays = [
         {
-          modelId: 1,
-          images: [IMAGES.img118, IMAGES.img122, IMAGES.img134, IMAGES.img142, IMAGES.img156, IMAGES.img165, IMAGES.img17,
+            modelId: 1,
+            images: [IMAGES.img118, IMAGES.img122, IMAGES.img134, IMAGES.img142, IMAGES.img156, IMAGES.img165, IMAGES.img17,
             IMAGES.img318, IMAGES.img320, IMAGES.img325, IMAGES.img328, IMAGES.img335, IMAGES.img345, IMAGES.img349,
             IMAGES.img27, IMAGES.img274, IMAGES.img276, IMAGES.img278
             ],
         },
         {
-          modelId: 2,
-          images: [IMAGES.img291, IMAGES.img299, IMAGES.img30, IMAGES.img300, IMAGES.img301, IMAGES.img305, IMAGES.img307, IMAGES.img312,
+            modelId: 2,
+            images: [IMAGES.img291, IMAGES.img299, IMAGES.img30, IMAGES.img300, IMAGES.img301, IMAGES.img305, IMAGES.img307, IMAGES.img312,
             IMAGES.img197, IMAGES.img205, IMAGES.img212, IMAGES.img213, IMAGES.img22, IMAGES.img222, IMAGES.img23, IMAGES.img240,
             IMAGES.img118, IMAGES.img122, IMAGES.img134, IMAGES.img142, IMAGES.img156
             ],
@@ -50,26 +45,56 @@ const Gallery = ({ models }) => {
                 IMAGES.img312, IMAGES.img118, IMAGES.img122, IMAGES.img134, IMAGES.img142, IMAGES.img156
             ],
         },
-      ];
-    const { id } = useParams();
-    const selectedModel = models.find(model => model.id === parseInt(id));
-  
-    if (!selectedModel) {
-      return <div>Model not found</div>;
+    ];
+
+    // Function to handle image selection
+    const handleImageClick = (image, index) => {
+        setSelectedImage(image);
+        setImageIndex(index);
+        setShowSlideshow(true);
+    };
+
+    const handleSlideshowPrev = () => {
+        setImageIndex((prevIndex) => (prevIndex === 0 ? selectedArray.images.length - 1 : prevIndex - 1));
+      };
+    
+      const handleSlideshowNext = () => {
+        setImageIndex((prevIndex) => (prevIndex === selectedArray.images.length - 1 ? 0 : prevIndex + 1));
+      };
+    
+      const closeSlideshow = () => {
+        setShowSlideshow(false);
+        setSelectedImage(null);
+        setImageIndex(0);
+      };
+
+    if (!imageArrays) {
+        return <div>No images available for this model</div>;
     }
 
+    if (!selectedModel) {
+        return <div>Model not found</div>;
+    }
+    
     const selectedArray = imageArrays.find((array) => array.modelId === selectedModel.id);
 
     return(
         <div className="container">
             <div className="img-grid">
-                {selectedArray.images.map((image, i) =>
-                <div className="img-wrap" key={i}>
-                    <img src={image} alt={i} 
-                    onClick={() => handleImageClick(image)}/>    
+                {selectedArray.images.map((image, index) =>
+                <div className="img-wrap" key={index}>
+                    <img src={image} alt={index} 
+                    onClick={() => handleImageClick(image, index)}/>    
                 </div>)}
                 {/* Render the Modal component if an image is selected */}
-                {selectedImage && <Modal selected={selectedImage} setSelected={clearSelectedImage}/>} 
+                {showSlideshow && (
+                    <Modal 
+                        selected={selectedArray.images[imageIndex]}
+                        setSelected={closeSlideshow}
+                        onPrev={handleSlideshowPrev}
+                        onNext={handleSlideshowNext}
+                />)}
+                {/*{selectedImage && <Modal selected={selectedImage} setSelected={clearSelectedImage}/>} */}
             </div>   
         </div>
     );
