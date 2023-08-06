420520430cd17d81a2a1170e15f5a0b4d4ef1aca
Meron95 <36710730+Meron95@users.noreply.github.com>
2023-08-06 18:37:51 +0200

    Update contact.jsx

--- src/componenets/contact/contact.jsx
+++ src/componenets/contact/contact.jsx
@@ -11,17 +11,17 @@ const Contact = () => {
                 <div className="col-md-4 col-sm-8" style={{paddingBottom: "50px"}}>
                     <img className="img_style" src={contactImg} alt="img12"/>
                 </div>
-            <div className="col-md-8 col-sm-12">
-                <p>You can reach me via email or it is also possible to call me for business inquiries.</p>
-                <p>
-                    <img src={phoneLogo} alt="phoneimg"/>
-                    <strong> 071234567</strong>
-                </p>
-                <p>
-                    <img src={worldLogo} alt="worldimg"/>
-                    <strong> godwinjonah0@gmail.com</strong>
-                </p>
-            </div>
+                <div className="col-md-8 col-sm-12">
+                    <p>You can reach me via email or it is also possible to call me for business inquiries.</p>
+                    <p>
+                        <img src={phoneLogo} alt="phoneimg"/>
+                        <strong> 071234567</strong>
+                    </p>
+                    <p>
+                        <img src={worldLogo} alt="worldimg"/>
+                        <strong> godwinjonah0@gmail.com</strong>
+                    </p>
+                </div>
             </div>
         </div>
     )
