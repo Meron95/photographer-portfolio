e44f8ce3b6f9db3cbe9a28a81228444f1a1e55fe
Meron95 <36710730+Meron95@users.noreply.github.com>
2023-08-06 18:40:53 +0200

    Update Modal.jsx

--- src/componenets/modals/Modal.jsx
+++ src/componenets/modals/Modal.jsx
@@ -1,15 +1,29 @@
 import React from "react";
 import './Modal.css';
 
-const Modal = ({ selected, setSelected }) => {
-  const handleClick = (e) => {
+const Modal = ({ selected, setSelected, onPrev, onNext }) => {
+  const handleBackdropClick = (e) => {
     if (e.target.classList.contains("backdrop")) {
       setSelected(null);
     }
   };
   return (
-    <div className="backdrop" onClick={handleClick}>
-      <img src={selected} alt="showing the pic" />
+    <div className="backdrop" onClick={handleBackdropClick}>
+      <div className="img-container">
+        <img src={selected} alt="showing the pic" />
+      </div>
+      <div className="buttons-container">
+          <button className="prev-button" onClick={onPrev}>
+            {"<"}
+          </button>
+          {/* Close button */}
+          <button className="close-button" onClick={() => setSelected(null)}>
+            X
+          </button>
+          <button className="next-button" onClick={onNext}>
+            {">"}
+          </button>
+        </div>
     </div>
   );
 };
