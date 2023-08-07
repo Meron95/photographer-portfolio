38e77084ed18269e367054f91586d5b116a74650
Meron95 <36710730+Meron95@users.noreply.github.com>
2023-08-07 17:48:01 +0200

    Delete src/models directory

--- src/models/export.js
+++
@@ -1,8 +0,0 @@
-const  ModelsList = {
-    img17: require("./17.jpg"),
-    img27: require("./27.jpg"),
-    img118: require("./118.jpg"),
-    img134: require("./134.jpg"),
-}
-
-export default ModelsList;
