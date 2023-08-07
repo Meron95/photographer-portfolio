324f5818c44ce4399a65322550ecaba21111dbe7
Meron95 <36710730+Meron95@users.noreply.github.com>
2023-08-07 17:49:28 +0200

    Add files via upload

---
+++ src/models/export.js
@@ -0,0 +1,8 @@
+const  ModelsList = {
+    img17: require("./17.jpg"),
+    img27: require("./27.jpg"),
+    img118: require("./118.jpg"),
+    img134: require("./134.jpg"),
+}
+
+export default ModelsList;
